58edafcf422441f9e4b89c846d3c617c5ed35125
gorani <akfmdl@naver.com>
2023-03-14 22:17:31 +0900

    fix: define TcpProxy

--- clearml_session/interactive_session_task.py
+++ clearml_session/interactive_session_task.py
@@ -14,6 +14,7 @@ import requests
 from clearml import Task, StorageManager
 from clearml.backend_api import Session
 from pathlib2 import Path
+from tcp_proxy import TcpProxy
 
 # noinspection SpellCheckingInspection
 default_ssh_fingerprint = {
